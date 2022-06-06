2183e962b45642cc50d6aed57fcfa95ef6d49d78
Kurt Dacudag <kurt.dacudag@sun-asterisk.com>
2022-06-06 13:32:03 +0800

    Display Laravel version

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
