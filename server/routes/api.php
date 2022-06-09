949b42ce1fc85050b4bb5ddedaea5b86194e307e
Kurt Dacudag <kurt.dacudag@sun-asterisk.com>
2022-06-09 17:47:59 +0800

    Add routes for followers

--- server/routes/api.php
+++ server/routes/api.php
@@ -20,4 +20,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/users', Api\UserController::class);
+
+    Route::get('/followers', [Api\FollowerController::class, 'index']);
+    Route::get('/followers/{user}', [Api\FollowerController::class, 'show']);
+    Route::post('/followers/{user}', [Api\FollowerController::class, 'store']);
+    Route::delete('/followers/{user}', [Api\FollowerController::class, 'destroy']);
+
+    Route::get('/following', [Api\FollowingController::class, 'index']);
+    Route::get('/following/{user}', [Api\FollowingController::class, 'show']);
 });
