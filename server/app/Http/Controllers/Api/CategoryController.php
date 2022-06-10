151689ae1636e6035ae1982a3ebf1451acc1e61f
Kurt Dacudag <kurt.dacudag@sun-asterisk.com>
2022-06-10 15:58:09 +0800

    Add category controller

---
+++ server/app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        return CategoryResource::collection(Category::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCategoryRequest  $request
+     * @return CategoryResource|\Illuminate\Http\JsonResponse
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $category = Category::create($request->validated());
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return CategoryResource
+     */
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCategoryRequest  $request
+     * @param  \App\Models\Category  $category
+     * @return CategoryResource|\Illuminate\Http\JsonResponse
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Category $category)
+    {
+        if (auth()->user()->is_admin) {
+            $category->delete();
+
+            return response()->json(['message' => 'Category deleted successfully.']);
+        } else {
+            return response()->json(['message' => 'You are not authorized to delete this resource.']);
+        }
+    }
+}
