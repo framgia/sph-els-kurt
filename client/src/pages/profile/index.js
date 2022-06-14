00b09ed3fcc9869edcfd6ed2b10fda38a5c62961
Kurt Dacudag <kurt.dacudag@sun-asterisk.com>
2022-06-14 09:52:49 +0800

    Add profile pages

---
+++ client/src/pages/profile/index.js
@@ -0,0 +1,69 @@
+import { connect, useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { fetchUsers } from "actions";
+import AppLayout from "components/layouts/AppLayout";
+import Loading from "components/layouts/Loading";
+
+const Profiles = (props) => {
+  const navigate = useNavigate();
+  const auth = useSelector((state) => state.auth);
+  useEffect(() => {
+    props.fetchUsers();
+  }, []);
+
+  const { users } = props;
+
+  if (!users.data || !users.data.length) {
+    return <Loading />;
+  }
+  console.log(auth.user.data.id);
+  const filteredUsers = users.data.filter((user) => {
+    return user.id !== auth.user.data.id;
+  });
+
+  return (
+    <AppLayout
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+          Profiles
+        </h2>
+      }
+    >
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        {filteredUsers.map((user) => (
+          <Link
+            to={`/profile/${user.id}`}
+            key={user.email}
+            className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+          >
+            <div className="flex-shrink-0">
+              <img
+                className="h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+            </div>
+            <div className="flex-1 min-w-0">
+              <div className="focus:outline-none">
+                <span className="absolute inset-0" aria-hidden="true" />
+                <p className="text-sm font-medium text-gray-900">{user.name}</p>
+                <p className="text-sm text-gray-500 truncate">{user.email}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </AppLayout>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+  };
+};
+
+export default connect(mapStateToProps, {
+  fetchUsers,
+})(Profiles);
---
+++ client/src/pages/profile/profile.js
@@ -0,0 +1,139 @@
+import { connect, useDispatch } from "react-redux";
+import { useParams } from "react-router";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  fetchUser,
+  fetchFollowers,
+  fetchFollowing,
+  followUser,
+  unfollowUser,
+} from "actions";
+import AppLayout from "components/layouts/AppLayout";
+import Loading from "components/layouts/Loading";
+
+const Profile = (props) => {
+  let { userId } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const follow = (id) => {
+    dispatch(followUser(id));
+
+    navigate("/");
+  };
+
+  const unfollow = (id) => {
+    dispatch(unfollowUser(id));
+
+    navigate("/");
+  };
+
+  useEffect(() => {
+    props.fetchUser(userId);
+    props.fetchFollowers(userId);
+    props.fetchFollowing(userId);
+  }, []);
+
+  if (
+    !props.user.data ||
+    !props.followers.data ||
+    !props.following.data ||
+    !props.auth
+  ) {
+    return <Loading />;
+  }
+
+  const { user } = props.auth;
+  const { is_admin, name, id } = props.user.data;
+  const { followers, following } = props;
+
+  if (is_admin === 1) {
+    navigate("/404");
+  }
+
+  const renderFollowButton = () => {
+    const isFound = followers.data.some((element) => {
+      if (element.id === user.data.id) {
+        return true;
+      }
+      return false;
+    });
+    if (isFound) {
+      return (
+        <button
+          onClick={() => unfollow(id)}
+          type="button"
+          className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-red-500"
+        >
+          Unfollow
+        </button>
+      );
+    }
+
+    if (id !== user.data.id) {
+      return (
+        <button
+          onClick={() => follow(id)}
+          type="button"
+          className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+        >
+          Follow
+        </button>
+      );
+    }
+  };
+
+  return (
+    <AppLayout
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+          Profile
+        </h2>
+      }
+    >
+      <div className="max-w-3xl mx-auto px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
+        <div className="flex items-center space-x-5">
+          <div className="flex-shrink-0">
+            <div className="relative">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                alt=""
+              />
+              <span
+                className="absolute inset-0 shadow-inner rounded-full"
+                aria-hidden="true"
+              />
+            </div>
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold text-gray-900">{name}</h1>
+            <div className="text-sm font-medium text-gray-500">
+              <div className="mr-4">Following: {following.data.length}</div>
+              <div className="mr-4">Followers: {followers.data.length}</div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
+          {renderFollowButton()}
+        </div>
+      </div>
+    </AppLayout>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+    user: state.users,
+    followers: state.followers,
+    following: state.following,
+  };
+};
+
+export default connect(mapStateToProps, {
+  fetchUser,
+  fetchFollowers,
+  fetchFollowing,
+})(Profile);
