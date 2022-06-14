7819348569e18dc45f29eed912f8118fb2ba703f
Kurt Dacudag <kurt.dacudag@sun-asterisk.com>
2022-06-14 10:28:55 +0800

    Add a sample user

--- server/database/seeders/UserSeeder.php
+++ server/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        //create user john doe
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com',
+            'password' => Hash::make('password'),
+            'is_admin' => false,
+        ]);
+
         User::factory()
             ->count(10)
             ->create();
