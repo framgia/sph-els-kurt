7ddaa0381c8e78051d36fa1471166b35318c0b02
Kurt Dacudag <kurt.dacudag@sun-asterisk.com>
2022-06-14 09:51:09 +0800

    Update combine reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import usersReducer from "./usersReducer";
+import followersReducer from "./followersReducer";
+import followingReducer from "./followingReducer";
 
 export default combineReducers({
   auth: authReducer,
+  users: usersReducer,
+  followers: followersReducer,
+  following: followingReducer,
 });
