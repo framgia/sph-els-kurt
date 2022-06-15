875e58ebbf3e74bf07eb0a4fd42b347dd1232474
Kurt Dacudag <kurt.dacudag@sun-asterisk.com>
2022-06-15 17:43:52 +0800

    Add page for categories

---
+++ client/src/pages/admin/categories/index.js
@@ -0,0 +1,110 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCategories } from "actions";
+import AppLayout from "components/layouts/AppLayout";
+import { Link, useNavigate } from "react-router-dom";
+import Loading from "components/Loading";
+import { PlusIcon } from "@heroicons/react/solid";
+import SuccessMessage from "../../../components/SuccessMessage";
+
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  if (!auth.user.data.is_admin) {
+    navigate("/404");
+  }
+  if (!categories.data) {
+    return <Loading />;
+  }
+
+  return (
+    <AppLayout
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+          Categories
+        </h2>
+      }
+    >
+      <div className="px-0">
+        <Link
+          to="/admin/categories/create"
+          className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+        >
+          Add category
+          <PlusIcon className="ml-3 -mr-1 h-5 w-5" aria-hidden="true" />
+        </Link>
+      </div>
+
+      {categories.message && (
+        <SuccessMessage className="mb-2" message={categories.message} />
+      )}
+
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-medium uppercase tracking-wide text-gray-500 sm:pl-6"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-medium uppercase tracking-wide text-gray-500 sm:pl-6"
+                    >
+                      Description
+                    </th>
+                    <th
+                      scope="col"
+                      className="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                    >
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white">
+                  {categories.data.map((category, categoryIdx) => (
+                    <tr
+                      key={category.name}
+                      className={
+                        categoryIdx % 2 === 0 ? undefined : "bg-gray-50"
+                      }
+                    >
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                        {category.name}
+                      </td>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-500 sm:pl-6">
+                        {category.description}
+                      </td>
+                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                        <a
+                          href="#"
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          Edit<span className="sr-only">, {category.name}</span>
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AppLayout>
+  );
+};
+
+export default Categories;
