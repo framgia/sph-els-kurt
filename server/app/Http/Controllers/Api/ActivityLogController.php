e68bbe3e3a024fbd18bdbdd9df9a04b12964aade
kurt <kurt.dacudag@sun-asterisk.com>
2022-07-04 12:31:08 +0800

    Fix where in activity log

--- server/app/Http/Controllers/Api/ActivityLogController.php
+++ server/app/Http/Controllers/Api/ActivityLogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ActivityLogResource;
 use App\Models\ActivityLog;
+use App\Models\User;
 use Spatie\Activitylog\Models\Activity;
 
 class ActivityLogController extends Controller
@@ -14,9 +15,11 @@ class ActivityLogController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(User $user)
     {
-        $activityLogs = Activity::get()->where('causer_id', auth()->id());
+        $activityLogs = Activity::where('causer_id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->get();
 
         return response()->json([
             'data' => ActivityLogResource::collection($activityLogs),
